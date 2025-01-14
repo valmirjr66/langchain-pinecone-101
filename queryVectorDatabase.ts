756cd57b3a29e7f4efc0eee77a620ca22ab4cd42
Valmir Júnior <valmirgmj@gmail.com>
2025-01-13 21:10:01 -0300

    New package to read prompt

--- queryVectorDatabase.ts
+++ queryVectorDatabase.ts
@@ -1,14 +1,12 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import * as dotenv from "dotenv";
 import queryPineconeAndGPT from "./queryPineconeAndGPT.js";
+import readline from 'readline-sync';
 
 dotenv.config();
-
-const PROMPT = "Who joined the first meeting with comp.coop?";
 const INDEX_NAME = `teste-index`;
-
 const client = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
 
-(async () => {
-  await queryPineconeAndGPT(client, INDEX_NAME, PROMPT);
-})();
+const question = readline.question("What would you like to ask?\n");
+
+(async () => await queryPineconeAndGPT(client, INDEX_NAME, question))();
